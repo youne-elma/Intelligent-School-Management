bd42f47b68b617aa22318c837316180882ff511e
NisrineDev <lasfernisrine@gmail.com>
2023-04-28 20:49:45 +0100

    Generate models to the project

--- ProjetPfe/settings.py
+++ ProjetPfe/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'ProjetPfe.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gEstion_fsa',
+        'NAME': 'gestion_fsa',
         'USER':'root',
         'PASSWORD':'',
         'HOST':'127.0.0.1',
