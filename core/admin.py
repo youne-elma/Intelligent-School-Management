75a123f505665d2604cf69df9167b4c32e83b836
younelma <youne.elma@gmail.com>
2023-05-16 20:41:33 +0100

    Model of admin panel page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from core.models import Etudiant, Professeur
+
 # Register your models here.
+
+@admin.register(Etudiant)
+class EtudiantAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Professeur)
+class ProfesseurAdmin(admin.ModelAdmin):
+    pass
