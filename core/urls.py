2e88138285f5245610c73803ab56b548aaff8153
unknown <eddevjo@gmail.com>
2023-05-24 03:57:01 +0100

    Added uploadInscirptionList + UploadFiliereList

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("gestionEtudiant/", views.gestionEtudiant, name="gestionEtudiant"),
-    path("gestionEtudiant/uplaodStudentList/", views.uplaodStudentList, name="uplaodStudentList"),
+    path("gestionEtudiant/uploadStudentList/", views.uplaodStudentList, name="uploadStudentList"),
+    path("gestionEtudiant/uploadInscriptionList/", views.uplaodInscriptionInfos, name="uploadInscriptionList")
+   ,path("gestionEtudiant/uploadEtudiantFiliere/", views.uploadEtudiantFiliere, name="uploadEtudiantFiliere")
 ]
--- core/views.py
+++ core/views.py
@@ -10,4 +10,10 @@ def gestionEtudiant(request):
     return render(request, 'core/gestionEtudiant.html')
 
 def uplaodStudentList(request):
-    return render(request, 'core/uplaodStudentList.html')
+    return render(request, 'core/uploadInscriptionList.html')
+
+def uplaodInscriptionInfos(request):
+    return render(request, 'core/uploadInscriptionInfos.html')
+
+def uploadEtudiantFiliere(request):
+    return render(request, 'core/uploadEtudiantFiliere.html')
