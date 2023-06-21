511b0cadbeba85f53db153f6a5c4a2652b76cbe6
mconr <moniruar05@gmail.com>
2023-06-21 03:25:34 +0200

    styling the password_reset page

--- core/urls.py
+++ core/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path("bugreportsuccess ", views.bugreportsuccess , name="bugreportsuccess "),
 
     #Reset Password Urlsss*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*
-    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name="resetpasswordlogin/password_reset_form.html"),name='password_reset'),
     path('password_reset/done',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
